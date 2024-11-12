dc3dc1ac2b295d60b017df3305a8c611c224b7ed
Wootae <comijoa2@naver.com>
2024-11-12 10:07:15 +0900

    Feat : RecommendationController 구현
    - 사용자의 추천 목록 반환하는 api 구현
    - TestData 넣는 메서드 작성

--- src/main/java/hello/roommate/RoommateApplication.java
+++ src/main/java/hello/roommate/RoommateApplication.java
@@ -1,8 +1,13 @@
 package hello.roommate;
 
+import hello.roommate.member.repository.MemberRepository;
+import hello.roommate.profile.repository.ProfileRepository;
+import hello.roommate.recommendation.repository.LifeStyleRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -14,4 +19,10 @@ public class RoommateApplication {
 		SpringApplication.run(RoommateApplication.class, args);
 	}
 
+	@Profile("local")
+	//@Bean
+	public TestDataInit testDataInit(MemberRepository memberRepository, LifeStyleRepository lifeStyleRepository,
+									 ProfileRepository profileRepository) {
+		return new TestDataInit(lifeStyleRepository, profileRepository, memberRepository);
+	}
 }
