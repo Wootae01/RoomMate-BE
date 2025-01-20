e22be86d4073f159af2b6b6cb34e60e0e62e99bb
wootae <kimwootae71@gmail.com>
2025-01-20 23:13:27 +0900

    Feat : 초기 테스트 데이터 생성하는 클래스 수정
    - LifeStyle 의 변경으로 초기 테스트 데이터를 저장해주는 클래스들을 생성하고 수정함

---
+++ src/main/java/hello/roommate/init/LifeStyleInit.java
@@ -0,0 +1,62 @@
+package hello.roommate.init;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Component;
+
+import hello.roommate.member.domain.Member;
+import hello.roommate.member.repository.MemberRepository;
+import hello.roommate.recommendation.domain.LifeStyle;
+import hello.roommate.recommendation.domain.Option;
+import hello.roommate.recommendation.domain.enums.Category;
+import hello.roommate.recommendation.repository.LifeStyleRepository;
+import hello.roommate.recommendation.repository.OptionRepository;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class LifeStyleInit {
+	private final LifeStyleRepository lifeStyleRepository;
+	private final OptionRepository optionRepository;
+	private final MemberRepository memberRepository;
+
+	public void createLifeStyle() {
+		List<Member> members = memberRepository.findAll();
+		List<Option> options = optionRepository.findAll();
+
+		List<LifeStyle> lifeStyles = new ArrayList<>();
+
+		for (Member member : members) {
+			List<Option> selected = selectOption(options);
+			for (Option option : selected) {
+				LifeStyle lifeStyle = new LifeStyle(option, member);
+				lifeStyles.add(lifeStyle);
+
+			}
+		}
+		lifeStyleRepository.saveAll(lifeStyles);
+	}
+
+	private List<Option> selectOption(List<Option> options) {
+		Map<Category, List<Option>> groupOption = options.stream()
+			.filter(option -> option.getCategory() != Category.AGE)
+			.collect(Collectors.groupingBy(Option::getCategory));
+
+		List<Option> selectOption = groupOption
+			.values().stream()
+			.map(list -> {
+				if (list == null || list.isEmpty()) {
+					return null;
+				}
+				return list.get(new Random().nextInt(list.size()));
+			})
+			.filter(Objects::nonNull)
+			.toList();
+		return selectOption;
+	}
+}
---
+++ src/main/java/hello/roommate/init/TestDataInit.java
@@ -0,0 +1,21 @@
+package hello.roommate.init;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class TestDataInit {
+	private final OptionInit optionInit;
+	private final MemberInit memberInit;
+	private final LifeStyleInit lifeStyleInit;
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void initData() {
+		optionInit.createOption();
+		memberInit.createMember();
+		lifeStyleInit.createLifeStyle();
+	}
+
+}
