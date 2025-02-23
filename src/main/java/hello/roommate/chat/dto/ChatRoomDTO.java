c32345522d80274b6083066f5f93f7297b3d42e3
wootae <kimwootae71@gmail.com>
2025-02-23 18:00:49 +0900

    chore: ChatRoomDTO 변수명 수정

--- src/main/java/hello/roommate/chat/dto/ChatRoomDTO.java
+++ src/main/java/hello/roommate/chat/dto/ChatRoomDTO.java
@@ -14,7 +14,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class ChatRoomDTO {
-	private Long id;                    //채팅방 id
+	private Long chatRoomId;            //채팅방 id
 	private LocalDateTime updatedTime;  //마지막 대화 날짜
 	private String message;             //마지막 대화 메시지
 	private String nickname;            //상대 닉네임
