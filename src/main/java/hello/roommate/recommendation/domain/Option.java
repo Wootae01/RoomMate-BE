b62285c495e474d855bb8b0323a52ddaf70b0fad
wootae <kimwootae71@gmail.com>
2025-02-24 21:56:32 +0900

    refactor: Option id 값 수동으로 관리하도록 변경

--- src/main/java/hello/roommate/recommendation/domain/Option.java
+++ src/main/java/hello/roommate/recommendation/domain/Option.java
@@ -1,8 +1,11 @@
 package hello.roommate.recommendation.domain;
 
 import hello.roommate.recommendation.domain.enums.Category;
-import jakarta.persistence.*;
-import lombok.AccessLevel;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,19 +15,18 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class Option {
-    @Id
-    @GeneratedValue
-    @Setter(AccessLevel.NONE)
-    @Column(name = "OPTION_ID")
-    private Long id;
+	@Id
+	@Column(name = "OPTION_ID")
+	private Long id;
 
-    @Enumerated(EnumType.STRING)  //enum 타입을 엔티티 클래스의 속성으로 사용위해, String : 값을 문자열로 DB에 저장
-    private Category category;
+	@Enumerated(EnumType.STRING)  //enum 타입을 엔티티 클래스의 속성으로 사용위해, String : 값을 문자열로 DB에 저장
+	private Category category;
 
-    private String optionValue;
+	private String optionValue;
 
-    public Option(Category category, String optionValue) {
-        this.category = category;
-        this.optionValue = optionValue;
-    }
+	public Option(Long id, Category category, String optionValue) {
+		this.category = category;
+		this.optionValue = optionValue;
+		this.id = id;
+	}
 }
