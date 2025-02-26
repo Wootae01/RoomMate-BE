708cd7e235b11fdb1384d17991f98ab8a63a1c16
LeeHuiYun <dude1599@naver.com>
2025-02-26 16:10:23 +0900

    feat: EditMemberDTO에 set.gender 가능하게 수정.

--- src/main/java/hello/roommate/auth/dto/EditMemberDTO.java
+++ src/main/java/hello/roommate/auth/dto/EditMemberDTO.java
@@ -1,6 +1,7 @@
 package hello.roommate.auth.dto;
 
 import hello.roommate.member.domain.Dormitory;
+import hello.roommate.member.domain.Gender;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -17,12 +18,14 @@ public class EditMemberDTO {
 	private String introduce;
 	private int age;
 	private Dormitory dormitory;
+	private Gender gender;
 
-	public EditMemberDTO(long userId, String nickname, String introduce, int age, Dormitory dormitory) {
+	public EditMemberDTO(long userId, String nickname, String introduce, int age, Dormitory dormitory, Gender gender) {
 		this.userId = userId;
 		this.nickname = nickname;
 		this.introduce = introduce;
 		this.age = age;
 		this.dormitory = dormitory;
+		this.gender = gender;
 	}
 }
