4e19938e5cbfa2615e6f5f9765e7d5df566795e5
wootae <kimwootae71@gmail.com>
2025-03-06 23:26:26 +0900

    feat: 나이 옵션 동적으로 생성

--- src/main/java/hello/roommate/recommendation/service/OptionService.java
+++ src/main/java/hello/roommate/recommendation/service/OptionService.java
@@ -1,26 +1,39 @@
 package hello.roommate.recommendation.service;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import hello.roommate.recommendation.domain.Option;
 import hello.roommate.recommendation.domain.enums.Category;
 import hello.roommate.recommendation.repository.OptionRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class OptionService {
 
-    private final OptionRepository optionRepository;
+	private final OptionRepository optionRepository;
 
-    public Option findByCategoryAndValue(Category category, String value) {
-        return optionRepository.findByCategoryAndValue(category, value);
-    }
+	public Option findByCategoryAndValue(Category category, String value) {
+		return optionRepository.findByCategoryAndValue(category, value);
+	}
 
-    public List<Option> findAll() {
-        return optionRepository.findAll();
-    }
+	/**
+	 * 년도 바뀌면 age option 추가
+	 */
+	@Scheduled(cron = "0 0 0 1 1 *")
+	protected void updateOption() {
+		int year = LocalDate.now().getYear();
+		Option option = new Option((long)year, Category.AGE, String.valueOf(year));
+		optionRepository.save(option);
+	}
+
+	public List<Option> findAll() {
+		return optionRepository.findAll();
+	}
 }
