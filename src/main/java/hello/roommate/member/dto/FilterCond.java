03e7b35ddbd803a074b5dcc64a43e07b55bf55c0
Wootae <comijoa2@naver.com>
2025-03-10 13:19:02 +0900

    feat: 필터 검증 로직 추가

--- src/main/java/hello/roommate/member/dto/FilterCond.java
+++ src/main/java/hello/roommate/member/dto/FilterCond.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 
 import hello.roommate.recommendation.domain.enums.Category;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,6 +16,7 @@ import lombok.Setter;
 @Setter
 public class FilterCond {
 
+	@NotNull(message = "{NotNull}")
 	private Map<Category, List<Long>> cond = new HashMap<>();
 
 }
