9a86cac4c2404ec6f80004ebaadefa098e00109a
wootae <kimwootae71@gmail.com>
2025-04-07 01:44:33 +0900

    fix: flyway db 테이블 명 소문자로 변경
    - linux 의 db 호환을 위해 테이블 명 소문자로 변경

--- src/main/resources/db/migration/V1__initial_schema.sql
+++ src/main/resources/db/migration/V1__initial_schema.sql
@@ -1,4 +1,5 @@
-CREATE TABLE MEMBER (
+
+CREATE TABLE member (
     member_id BIGINT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(255),
     nickname VARCHAR(10),
@@ -8,39 +9,39 @@ CREATE TABLE MEMBER (
     gender VARCHAR(30)
 );
 
-CREATE TABLE CHAT_ROOM(
+CREATE TABLE chat_room(
     chat_room_id BIGINT PRIMARY KEY AUTO_INCREMENT
 );
 
-CREATE TABLE MEMBER_CHAT_ROOM(
+CREATE TABLE member_chat_room(
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
     member_id BIGINT,
     chat_room_id BIGINT,
-    FOREIGN KEY (member_id) REFERENCES MEMBER(member_id),
-    FOREIGN KEY (chat_room_id) REFERENCES CHAT_ROOM(chat_room_id)
+    FOREIGN KEY (member_id) REFERENCES member(member_id),
+    FOREIGN KEY (chat_room_id) REFERENCES chat_room(chat_room_id)
 
 );
 
-CREATE TABLE MESSAGE(
+CREATE TABLE message(
     message_id BIGINT PRIMARY KEY AUTO_INCREMENT,
     sender_id BIGINT,
     chat_room_id BIGINT,
     content TEXT,
-    sendTime DATETIME,
-    FOREIGN KEY (sender_id) REFERENCES MEMBER(member_id),
-    FOREIGN KEY (chat_room_id) REFERENCES CHAT_ROOM(chat_room_id)
+    send_time DATETIME,
+    FOREIGN KEY (sender_id) REFERENCES member(member_id),
+    FOREIGN KEY (chat_room_id) REFERENCES chat_room(chat_room_id)
 
 );
 
-CREATE TABLE NOTIFICATION(
+CREATE TABLE notification(
     notification_id BIGINT PRIMARY KEY AUTO_INCREMENT,
     member_id BIGINT,
     token VARCHAR(255),
     permission TINYINT(1),
-    FOREIGN KEY (member_id) REFERENCES MEMBER(member_id)
+    FOREIGN KEY (member_id) REFERENCES member(member_id)
 );
 
-CREATE TABLE REFRESH_ENTITY(
+CREATE TABLE refresh_entity(
     refresh_id BIGINT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(255),
     refresh VARCHAR(511),
@@ -48,27 +49,27 @@ CREATE TABLE REFRESH_ENTITY(
     expiration TIMESTAMP
 );
 
-CREATE TABLE OPTIONS(
+CREATE TABLE options(
     option_id BIGINT PRIMARY KEY ,
     category VARCHAR(255),
     option_value VARCHAR(255)
 );
 
-CREATE TABLE LIFESTYLE(
+CREATE TABLE lifestyle(
     lifestyle_id BIGINT PRIMARY KEY AUTO_INCREMENT,
     option_id BIGINT,
     member_id BIGINT,
 
-    FOREIGN KEY (option_id) REFERENCES OPTIONS(option_id),
-    FOREIGN KEY (member_id) REFERENCES MEMBER(member_id)
+    FOREIGN KEY (option_id) REFERENCES options(option_id),
+    FOREIGN KEY (member_id) REFERENCES member(member_id)
 );
 
-CREATE TABLE PREFERENCE(
+CREATE TABLE preference(
     preference_id BIGINT PRIMARY KEY AUTO_INCREMENT,
     option_id BIGINT,
     member_id BIGINT,
 
-    FOREIGN KEY (option_id) REFERENCES OPTIONS(option_id),
-    FOREIGN KEY (member_id) REFERENCES MEMBER(member_id)
+    FOREIGN KEY (option_id) REFERENCES options(option_id),
+    FOREIGN KEY (member_id) REFERENCES member(member_id)
 )
 
