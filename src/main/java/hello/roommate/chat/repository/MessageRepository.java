4747cc039ef2bf93dc5f47eba568701bceeb3c85
wootae <comijoa2@naver.com>
2026-03-24 15:51:19 +0900

    fix: 채팅 메시지 조회 시 정렬 순서 명시

--- src/main/java/hello/roommate/chat/repository/MessageRepository.java
+++ src/main/java/hello/roommate/chat/repository/MessageRepository.java
@@ -12,7 +12,7 @@ import hello.roommate.chat.domain.Message;
 public interface MessageRepository extends JpaRepository<Message, Long> {
 
 	//해당 채팅방의 모든 메시지 찾아서 반환
-	@Query("select m from Message m where m.chatRoom.id = :id")
+	@Query("select m from Message m where m.chatRoom.id = :id order by m.sendTime asc")
 	List<Message> findAllByChatRoomId(@Param("id") Long chatRoomId);
 
 	//가장 최근 메시지 찾아서 반환
